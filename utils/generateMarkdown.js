5cea1c73df26fdde1622b815b39e6d686feb9ddc
Barrerse <sebastianbarrera@live.com>
2023-01-11 23:27:39 -0500

    added license functions and finished the markdown generator

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+  if (license !== 'no license') {
+    return `
+  ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+  return `
+  [${license}](https://choosealicense.com/licenses/${license})
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+  return `
+  ## [License](#table-of-contents)
+  The application is covered under the following license:
+  ${renderLicenseLink(license)}
+    `;
+  } else {
+    return ' ';
+  }
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -16,19 +42,47 @@ function generateMarkdown(data) {
   
   # ${data.title}
 
-  ${renderLicenseTOC(data.license)}
+  ${renderLicenseBadge(data.license)}
 
   ## Table of Contents 
-  ${renderLicenseTOC(data.license)}
   * [Description](#description)
   * [Installation](#installation)
   * [Usage](#usage)
+  * [License](#license)
   * [Contributing](#contributing)
   * [Tests](#tests)
   * [Questions](#questions)
 
-  ##
+  ## [Description]
 
+  ${data.description}
+
+  ## [Installation]
+
+  ${data.installation}
+
+  ## [Usage]
+
+  ${data.usage}
+  
+  To see a visual example on how to use the README generator pelase watch the video below:
+  
+  [Check out this video](https://agea.github.io/tutorial.md/)
+
+  ${renderLicenseSection(data.license)}
+  
+  ## [Contributing]
+  
+  ${data.contributing}
+
+  ## [Tests]
+
+  ${data.test}
+
+  ## [Questions]
+  For anu questions contact me using the following links:
+  [GitHub](https://github.com/${data.githubUsername})
+  [Email: ${data.email}](mailto:${data.email})
 `;
 }
 
